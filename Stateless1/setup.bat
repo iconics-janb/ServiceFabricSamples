78ff1703f35b7b1be6ff7f71a3a5d29e6654db4f
Bob Jacobs <jacobsrj@hotmail.com>
2016-11-14 18:35:14 -0800

    fixing DCOM deployment issues

--- Stateless1/setup.bat
+++ Stateless1/setup.bat
@@ -1,1 +1,15 @@
-DcomModule.exe /regserver
+set curdir=%cd%\..\Stateless1Pkg.Code.1.0.0
+set checkfile=%curdir%\DcomModule.exe
+
+echo %checkfile%
+if exist %checkfile% (
+	echo "it exists"
+	setx -m TestVariable "FileExists"
+	%checkfile% /regserver
+) else (
+	echo "does not exist"
+	setx -m TestVariable %checkfile%
+)
+
+
+
